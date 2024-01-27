55a5a8efbe434682b952ff0f8ac7fa2066f5f47b
madiber <mario.diber@gmail.com>
2024-01-27 12:03:25 +0100

    mostro, parte_mostro

---
+++ components/mostro/parte_mostro/parte_mostro.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		print("Mouse Click/Unclick at: ", event.position)
+	elif event is InputEventMouseMotion:
+		print("Mouse Motion at: ", event.position)
+
+	# Print the size of the viewport.
+	print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
+
