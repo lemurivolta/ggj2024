335b64fec3a9c86bdfdf5fe16bb92adcf871b75f
Mattia Belletti <mbelletti@greenteam.it>
2024-01-27 12:01:18 +0100

    feat: global bus

--- components/global_bus/global_bus.gd
+++ components/global_bus/global_bus.gd
@@ -1,11 +1,50 @@
 extends Node
 
+###############################################################################
+# CONFIGURATION
+###############################################################################
 
-# Called when the node enters the scene tree for the first time.
+# Current level of pleasure
+var current_pleasure: float = 0
+
+# Level of pleasure under which the monster is enraged and go to gameover
+var enraged_threshold = -5
+
+# Level of pleasure over which the monster is really happy and gets quiet
+var happy_threshold = 5
+
+###############################################################################
+# SIGNALS
+###############################################################################
+
+# Signal raised whenever the monster is tickled in a good place (positive) or
+# bad place (negative)
+signal tickle(pleasure: float)
+
+# Signal raised when the monster is too displeased and gets enraged (gameover)
+signal enraged
+
+# Signal raised when the monster is really happy and gets quiet
+signal happy
+
+###############################################################################
+# FUNCTIONS
+###############################################################################
+
+# Reset the pleasure level to 0
+func reset_pleasure():
+	current_pleasure = 0
+
+###############################################################################
+# INTERNALS
+###############################################################################
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	tickle.connect(_on_tickle)
+	
+func _on_tickle(value: float):
+	current_pleasure += value
+	if current_pleasure <= enraged_threshold:
+		enraged.emit()
+	elif current_pleasure >= happy_threshold:
+		happy.emit()
