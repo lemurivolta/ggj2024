09020b3971ba276fcb216d515b374dca6c24c023
Mattia Belletti <mbelletti@greenteam.it>
2024-01-27 13:27:42 +0100

    feat: happy and enraged laughter

--- components/laugh_or_grr/laugh_or_grr.gd
+++ components/laugh_or_grr/laugh_or_grr.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var sprites_happy: Array[Sprite2D]
+@export var sprites_enraged: Array[Sprite2D]
 @export var random_rotation_radians: float = PI / 6
 
 # Called when the node enters the scene tree for the first time.
@@ -9,10 +10,13 @@ func _ready():
 
 
 func _on_tickle(_pleasure):
-	var happy_level = clamp(GlobalBus.current_pleasure, 0, GlobalBus.happy_threshold)
-	var num_happy_enabled = \
-		ceil(happy_level / GlobalBus.happy_threshold * sprites_happy.size())
-	for i in sprites_happy.size():
-		sprites_happy[i].visible = i < num_happy_enabled
-		sprites_happy[i].rotation = \
+	_update_sprites(GlobalBus.happy_threshold, 0, GlobalBus.happy_threshold, sprites_happy)
+	_update_sprites(GlobalBus.enraged_threshold, GlobalBus.enraged_threshold, 0, sprites_enraged)
+
+func _update_sprites(threshold: float, min: float, max: float, sprites: Array[Sprite2D]):
+	var level = clamp(GlobalBus.current_pleasure, min, max)
+	var num_enabled = ceil(level / threshold * sprites.size())
+	for i in sprites.size():
+		sprites[i].visible = i < num_enabled
+		sprites[i].rotation = \
 			randf() * random_rotation_radians - (random_rotation_radians / 2)
