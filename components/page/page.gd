28e4f4b451b376e10d9b314b7e53f40c5e2d2775
Mattia Belletti <mbelletti@greenteam.it>
2024-01-27 18:46:25 +0100

    feat: page flip cubic tween

--- components/page/page.gd
+++ components/page/page.gd
@@ -4,8 +4,10 @@ extends Node2D
 
 func flip_away():
 	GlobalBus.tickle_enabled = false
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "scale:x", 0, flip_duration)
+	var tween: Tween = get_tree().create_tween()
+	var scale_tweener: PropertyTweener = \
+		tween.tween_property(self, "scale:x", 0, flip_duration)
+	scale_tweener.set_trans(Tween.TRANS_CUBIC)
 	tween.tween_callback(self.queue_free)
 	tween.tween_callback(self._re_enable_tickle)
 	
