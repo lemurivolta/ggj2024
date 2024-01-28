03d3539f225fc98f2ff692f5614efcf9833c021a
Mattia Belletti <mbelletti@greenteam.it>
2024-01-28 10:47:49 +0100

    feat: main menu

--- components/root/root.gd
+++ components/root/root.gd
@@ -11,15 +11,15 @@ func _on_menu_start_game():
 	main = main_scene.instantiate()
 	add_child(main)
 	var tween: Tween = get_tree().create_tween()
-	tween.tween_property(menu, "scale:x", 0, 1)
+	var scale_tweener: PropertyTweener = tween.tween_property(menu, "scale:x", 0, 1)
+	scale_tweener.set_trans(Tween.TRANS_CUBIC)
 
 func _on_enraged():
-	menu.z_index = 0
-	menu.scale.x = 1
 	var tween: Tween = get_tree().create_tween()
-	tween.tween_property(main, "scale:x", 0, 1)
+	var scale_tweener: PropertyTweener = tween.tween_property(menu, "scale:x", 1, 1)
+	scale_tweener.set_trans(Tween.TRANS_CUBIC)
+	scale_tweener.set_ease(Tween.EASE_OUT)
 	tween.tween_callback(main.queue_free)
-	tween.tween_callback(_menu_to_top)
 
-func _menu_to_top():
-	menu.z_index = 1
+#func _menu_to_top():
+	#menu.z_index = 1
